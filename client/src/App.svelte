abd4dcb177137f61a9a2820e8e8d5b352923ada1
leomet07 <leomet07@gmail.com>
2021-06-20 21:26:05 -0400

    Added multiple pages, single page app config change, added font

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -3,20 +3,32 @@
 	import { Router, Link, Route } from "svelte-routing";
 	import Home from "./routes/Home.svelte";
 	import About from "./routes/About.svelte";
-	
-  
+	import FAQS from "./routes/FAQS.svelte";
+	import Account from "./routes/Account.svelte";
+	import Questions from "./routes/Questions.svelte";
+
 	export let url = "";
 </script>
-  
-  <Router url="{url}">
+
+<Router {url}>
 	<nav>
-	  <Link to="/">Home</Link>
-	  <Link to="about">About</Link>
-	  
+		<Link to="/">Home</Link> |
+		<Link to="faqs">Faqs</Link> |
+		<Link to="questions">Questions</Link> |
+		<Link to="account">Account</Link> |
+		<Link to="about">About</Link>
 	</nav>
 	<div>
-	  
-	  <Route path="about" component="{About}" />
-	  <Route path="/"><Home /></Route>
+		<Route path="about" component={About} />
+		<Route path="faqs" component={FAQS} />
+		<Route path="questions" component={Questions} />
+		<Route path="account" component={Account} />
+		<Route path="/"><Home /></Route>
 	</div>
-  </Router>
+</Router>
+
+<style>
+	nav {
+		text-align: center;
+	}
+</style>
--- client/src/routes/Home.svelte
+++ client/src/routes/Home.svelte
@@ -1,1 +1,10 @@
-<h1>Home</h1>
+<main id="home">
+	<h1>Home</h1>
+	<h2>A FAQ page for tech newbies (and dummies)</h2>
+</main>
+
+<style>
+	#home {
+		text-align: center;
+	}
+</style>
