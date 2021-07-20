d68c28674d0b882fd75f24d119e56ea8030be435
leomet07 <leomet07@gmail.com>
2021-07-20 17:28:09 -0400

    Front end move

--- client/src/routes/Questions.svelte
+++ client/src/routes/Questions.svelte
@@ -1,2 +1,42 @@
-<h1>Questions</h1>
-<h2>Ask dumb questions, like in a forum!</h2>
+<script>
+	let questions = [];
+	import { validauthtoken } from "../stores";
+	(async () => {
+		validauthtoken.subscribe(async (val) => {
+			if (val != "") {
+				questions = await get_questions({}, val);
+				console.log(questions);
+			}
+		});
+	})();
+	async function get_questions(body, auth_token) {
+		let response = await fetch(window.BASE_URL + "/api/db/get_questions", {
+			method: "POST",
+			body: JSON.stringify(body),
+			headers: {
+				"Content-Type": "application/json",
+				"auth-token": auth_token,
+			},
+		});
+		let json = await response.json();
+		return json.questions;
+	}
+</script>
+
+<main id="questions_main">
+	<h1>Questions</h1>
+	{#each questions as question}
+		<h4>
+			Question: {question.question}
+		</h4>
+		<h4>
+			Answer: {question.answer}
+		</h4>
+	{/each}
+</main>
+
+<style>
+	#questions_main {
+		text-align: center;
+	}
+</style>
